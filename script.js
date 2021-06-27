360f164c005256c35bfc202e16773c7ad1bd1140
huahuuchi <86546904+huahuuchi@users.noreply.github.com>
2021-06-27 13:28:20 +0700

    My first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/* First - change the active panel*/
+/* Bring in all of these panels into JavaScript*/
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click',() => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+    })
+}
